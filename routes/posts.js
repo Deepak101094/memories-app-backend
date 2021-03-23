3c544f9a14d03523cd7ed71e81c0146ed5f8380e
Deepak101094 <deepakpandey101094@gmail.com>
2021-03-23 19:08:50 +0530

    created routes for get and post request

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 // setup the starting path for all routes from this file, that is localhost:5000/posts
 router.get("/", getPosts);
+router.post("/", createPost )
 
 export default router;
